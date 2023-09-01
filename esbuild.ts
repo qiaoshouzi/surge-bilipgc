3772494b0638cf2717e018173e4f087d2d41e2d8
qiaoshouzi <qiaoshouzi@cfm.moe>
2023-09-01 20:10:58 +0800

    update

--- esbuild.ts
+++ esbuild.ts
@@ -1,6 +1,7 @@
 import { BuildOptions, buildSync } from "esbuild";
 import fs from "node:fs";
 import path from "node:path";
+import crypto from "node:crypto";
 
 const deleteFolderRecursive = (folderPath: string): void => {
   if (fs.existsSync(folderPath)) {
@@ -43,11 +44,36 @@ try {
     inject: ["./src/text-polyfill.js"],
   });
 
+  // hash
+  const fileNameList = fs.readdirSync(path.join(__dirname, "dist")).filter((v) => v.endsWith(".js"));
+  let newFileNameList: string[] = [];
+  for (const fileName of fileNameList) {
+    const rawFilePath = path.join(__dirname, "dist", fileName);
+    const raw = fs.readFileSync(rawFilePath, "utf8");
+    const hash = crypto.createHash("sha256").update(raw).digest("hex").slice(0, 10);
+    const newFileName = `${fileName.replace(/\.js$/, "")}.${hash}.js`;
+    const newFilePath = path.join(__dirname, "dist", newFileName);
+    fs.renameSync(
+      rawFilePath,
+      newFilePath
+    );
+    newFileNameList.push(newFileName);
+    const fileSize = fs.statSync(newFilePath).size / 1024
+    console.log(`${newFileName}   ${fileSize.toFixed(1)}kb`);
+  }
+
   // .sgmodule
+  let sgmoduleRaw = fs.readFileSync(path.join(__dirname, "./BiliPGC.sgmodule"), "utf8");
+  for (const i in fileNameList) {
+    const fileName = fileNameList[i];
+    const newFileName = newFileNameList[i];
+    sgmoduleRaw = sgmoduleRaw.replace(
+      `https://bilipgc.cfm.moe/${fileName}`, `https://bilipgc.cfm.moe/${newFileName}`
+    );
+  }
   fs.writeFileSync(
-    path.resolve(__dirname, "./dist/BiliPGC.sgmodule"),
-    fs.readFileSync(path.resolve(__dirname, "./BiliPGC.sgmodule"), "utf-8"),
-    "utf-8"
+    path.join(__dirname, "dist", "BiliPGC.sgmodule"),
+    sgmoduleRaw, "utf8"
   );
 } catch (e) {
   process.stderr.write(e.stderr);
